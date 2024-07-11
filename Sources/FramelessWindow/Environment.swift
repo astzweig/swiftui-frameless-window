db1f49739371c01c1acaa903caa22f32fafba33f
T. R. Bernstein <137705289+trbernstein@users.noreply.github.com>
2024-07-11 10:00:23 +0200

    Use tabs for indention

--- Sources/FramelessWindow/Environment.swift
+++ Sources/FramelessWindow/Environment.swift
@@ -2,20 +2,20 @@ import SwiftUI
 
 /// The internal storage environment key for the frameless window.
 private struct FramelessWindowEnvironmentKey: EnvironmentKey {
-    static let defaultValue: Binding<NSWindow?> = .constant(nil)
+	static let defaultValue: Binding<NSWindow?> = .constant(nil)
 }
 
 /// The environment key for accessing the frameless window.
 public extension EnvironmentValues {
-    var framelessWindow: Binding<NSWindow?> {
-        get { self[FramelessWindowEnvironmentKey.self] }
-        set { self[FramelessWindowEnvironmentKey.self] = newValue }
-    }
+	var framelessWindow: Binding<NSWindow?> {
+		get { self[FramelessWindowEnvironmentKey.self] }
+		set { self[FramelessWindowEnvironmentKey.self] = newValue }
+	}
 }
 
 /// The view modifier for more concise write access to the framelessWindow environment value.
 public extension View {
-    func framelessWindow(_ window: Binding<NSWindow?>) -> some View {
-        self.environment(\.framelessWindow, window)
-    }
+	func framelessWindow(_ window: Binding<NSWindow?>) -> some View {
+		self.environment(\.framelessWindow, window)
+	}
 }
