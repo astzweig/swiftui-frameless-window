b4c31fe0a32ab3abd113f4717230bd631d5db245
T. R. Bernstein <137705289+trbernstein@users.noreply.github.com>
2024-07-11 16:17:05 +0200

    Add windowInitializer option

--- Sources/FramelessWindow/FramelessWindow.swift
+++ Sources/FramelessWindow/FramelessWindow.swift
@@ -8,9 +8,12 @@ import WindowReference
  default window buttons. Additionally it is excluded from the applications window menu.
  */
 public struct FramelessWindow<Content: View>: Scene {
+	public typealias WindowInitializer = (NSWindow) -> Void
+
 	let titleKey: LocalizedStringKey
 	let id: String
 	let content: () -> Content
+	let windowInitializer: WindowInitializer
 
 	/// A `Window` scene with a content that ignores any safe area and puts a window reference
 	/// into the environment.
@@ -27,16 +30,42 @@ public struct FramelessWindow<Content: View>: Scene {
 
 	 - Parameters:
 	 - id: A unique string identifier that you can use to open the window.
-	 - titleKey: A localized string key to use for the window’s title in system menus and in the
+	 - title: A localized string key to use for the window’s title in system menus and in the
 	 window’s title bar. Provide a title that describes the purpose of the window. As
 	 a frameless window does not have a title bar and does not appear in the application
-	 window menu, this value has no effect. Default value is `"Launcher"`.
+	 window menu, this value has no effect.
+	 - windowInitializer: Closure to modify the containing NSWindow of the scene.
 	 - content: The view content to display in the window.
 	 */
-	public init(_ title: LocalizedStringKey = "Launcher", id: String, @ViewBuilder content: @escaping () -> Content) {
+	public init(_ title: LocalizedStringKey, id: String, windowInitializer: WindowInitializer? = nil, @ViewBuilder content: @escaping () -> Content) {
 		self.titleKey = title
 		self.id = id
 		self.content = content
+		self.windowInitializer = windowInitializer ?? {window in}
+	}
+
+	/**
+	 Initialize a new FramelessWindow Scene without a title, which defaults to `"Launcher"`.
+
+	 - Parameters:
+	 - id: A unique string identifier that you can use to open the window.
+	 - windowInitializer: Closure to modify the containing NSWindow of the scene.
+	 - content: The view content to display in the window.
+	 */
+	public init(id: String, windowInitializer: WindowInitializer? = nil, @ViewBuilder content: @escaping () -> Content) {
+		self.init("Launcher", id: id, windowInitializer: windowInitializer, content: content)
+	}
+
+	/**
+	 Initialize a new FramelessWindow Scene without a title, which defaults to `"Launcher"` and id.
+	 id defaults to some random UUID.
+
+	 - Parameters:
+	 - windowInitializer: Closure to modify the containing NSWindow of the scene.
+	 - content: The view content to display in the window.
+	 */
+	public init(windowInitializer: WindowInitializer? = nil, @ViewBuilder content: @escaping () -> Content) {
+		self.init(id: UUID().uuidString, windowInitializer: windowInitializer, content: content)
 	}
 
 	/// Hide the miniaturize and zoom default window buttons and remove the window from the application window menu.
@@ -44,5 +73,6 @@ public struct FramelessWindow<Content: View>: Scene {
 		window.standardWindowButton(.miniaturizeButton)?.isHidden = true
 		window.standardWindowButton(.zoomButton)?.isHidden = true
 		window.isExcludedFromWindowsMenu = true
+		self.windowInitializer(window)
 	}
 }
--- Sources/TestApp/main.swift
+++ Sources/TestApp/main.swift
@@ -3,12 +3,16 @@ import FramelessWindow
 
 struct TestApp: App {
 	var body: some Scene {
-		FramelessWindow("Single Window", id: "single-window") {
+		FramelessWindow("Single Window", id: "single-window", windowInitializer: self.modify(window:)) {
 			VStack {
 				Image(systemName: "globe").font(.title).foregroundStyle(.blue)
 				Text("Hello, world!")
 			}
 		}
+	}
+
+	func modify(window: NSWindow) {
+		print("Modify window of TestApp was called successfully.")
 	}
 }
 
